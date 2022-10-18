ba97f05ad06c71a7c7a6db1ae05549237dfad4c1
sergio <isergioinf@gmail.com>
2022-10-18 17:18:30 -0500

    finalizado

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask_cors import CORS
+
+
 from config import config
 
 # Routes
@@ -6,11 +9,14 @@ from routes import Movie
 
 app=Flask(__name__)
 
+CORS(app, resources={"*":{"origins":"htpp://localhost:9300"}})
+
 def page_not_found(error):
     return "<h1>Not found page<h1>",404
 
 if __name__=='__main__':
     app.config.from_object(config['development'])
+
     #Blueprints
     app.register_blueprint(Movie.main,url_prefix='/api/movies')
     # Error handlers 
